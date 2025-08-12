0cd14c3db59919ca3350b9e82ab9f111e623aa9a
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2025-08-12 11:57:38 -0300

    Label envolvendo input

--- formulario/form/src/App.jsx
+++ formulario/form/src/App.jsx
@@ -2,15 +2,11 @@ import MyForm from './components/MyForm'
 
 import './App.css'
 
-
-
 function App() {
-
-
   return (
     <div className='App'>
       <h1>Form em Reat</h1>
-      <MyForm/>
+      <MyForm />
     </div>
   )
 }
