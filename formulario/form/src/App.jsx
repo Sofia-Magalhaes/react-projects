f4e2734cf7086bdea0432e9abfaf25d6f0e34598
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2025-08-12 12:35:02 -0300

    Introdução aos controlled inputs
    controlled input podemos atribuir valores pré-existentes aos inputs dos forms

--- formulario/form/src/App.jsx
+++ formulario/form/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className='App'>
       <h1>Form em Reat</h1>
-      <MyForm />
+      <MyForm userName="Sofia" userEmail="sofia@gmail.com"/>
     </div>
   )
 }
--- formulario/form/src/components/MyForm.jsx
+++ formulario/form/src/components/MyForm.jsx
@@ -2,20 +2,20 @@ import './MyForm.css'
 
 import { useEffect, useState } from 'react'
 
-const MyForm = () => {
+const MyForm = ({userName, userEmail}) => {
     // Gerenciamento de dados
-    const [name, setName] = useState()
-    const [email, setEmail] = useState()
+    const [name, setName] = useState(userName) 
+    const [email, setEmail] = useState(userEmail)
 
     const handleName = (e) => {
         setName(e.target.value)
     }
 
     // Envio de form
-    const handleSubmit = (e) =>{
+    const handleSubmit = (e) => {
         e.preventDefault()
         console.log(name, email);
-        
+
     }
 
     // console.log(name, email);
@@ -24,18 +24,25 @@ const MyForm = () => {
     return (
         <div>
             {/* Criação do form */}
-            {/* Envio de formualario */}            
+            {/* Envio de formualario */}
             <form onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="name">Nome: </label>
-                    <input type="text" name="name" placeholder='Digite o seu nome' onChange={handleName} />
+                    <input type="text" name="name" placeholder='Digite o seu nome'
+                        onChange={handleName}
+                        // Controlled Input
+                        value={name}
+                    />
                 </div>
                 {/* Label envolvendo input */}
                 <label>
                     <span>E-mail:</span>
                     <input type="text" name="email" placeholder='Digite o seu e-mail'
                         // simpliifcando manipulação
-                        onChange={(e) => setEmail(e.target.value)} />
+                        onChange={(e) => setEmail(e.target.value)}
+                        // Controlled Input
+                        value={email || ""}
+                    />
                 </label>
                 <input type="submit" value="Enviar" />
             </form>
