44b3c222e0e9aea379bd54071189b863332953c1
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2025-08-12 12:09:42 -0300

    Salvando dado de input em um state
    usando state para alterar o input

--- formulario/form/src/components/MyForm.jsx
+++ formulario/form/src/components/MyForm.jsx
@@ -1,23 +1,36 @@
 import './MyForm.css'
 
+import { useEffect, useState } from 'react'
+
 const MyForm = () => {
-  return (
-    <div>
-        {/* Criação do form */}
-        <form>
-            <div>
-                <label htmlFor="name">Nome: </label>
-                <input type="text" name="name" placeholder='Digite o seu nome'/>
-            </div>
-            {/* Label envolvendo input */}
-            <label>
-                <span>E-mail:</span>
-                <input type="text" name="email" placeholder='Digite o seu e-mail' />
-            </label>
-            <input type="submit" value="Enviar" />
-        </form>
-    </div>
-  )
+    // Gerenciamento de dados
+    const [name, setName] = useState()
+    const [email, setEmail] = useState()
+
+    const handleName = (e) =>{
+        setName(e.target.value)
+    }
+
+    console.log(name);
+    
+
+    return (
+        <div>
+            {/* Criação do form */}
+            <form>
+                <div>
+                    <label htmlFor="name">Nome: </label>
+                    <input type="text" name="name" placeholder='Digite o seu nome' onChange={handleName} />
+                </div>
+                {/* Label envolvendo input */}
+                <label>
+                    <span>E-mail:</span>
+                    <input type="text" name="email" placeholder='Digite o seu e-mail' />
+                </label>
+                <input type="submit" value="Enviar" />
+            </form>
+        </div>
+    )
 }
 
 export default MyForm
