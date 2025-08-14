26dd112087c125a2353405198a9feaefe82b3f37
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2025-08-14 11:30:09 -0300

    Criando hook principal

--- form-multi-step/multistep_form_react/src/App.jsx
+++ form-multi-step/multistep_form_react/src/App.jsx
@@ -1,9 +1,20 @@
 // Components
-import {GrFormNext, GrFormPrevious} from 'react-icons/gr'
+import { GrFormNext, GrFormPrevious } from 'react-icons/gr'
+import UserForm from './components/UserForm'
+import ReviewForm from './components/ReviewForm'
+import Thanks from './components/Thanks'
+
+
+//Hooks
+import { useForm } from './hooks/useForm'
 
 import './App.css'
 
 function App() {
+
+  const formComponents = [<UserForm />, <ReviewForm />, <Thanks />]
+
+  const {currentStep, currentComponent} = useForm(formComponents)
 
   return (
     <div className='app'>
@@ -14,15 +25,15 @@ function App() {
       <div className="form-container">
         <p>etapas</p>
         <form>
-          <div className="inputs-container"></div>
+          <div className="inputs-container">{currentComponent}</div>
           <div className="action">
             <button type='button'>
-              <GrFormPrevious/>
+              <GrFormPrevious />
               <span>Voltar</span>
             </button>
             <button type='submit'>
               <span>Avançar</span>
-              <GrFormNext/>
+              <GrFormNext />
             </button>
           </div>
         </form>
