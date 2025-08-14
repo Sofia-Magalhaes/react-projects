493558c540bb2cc8ba5d87c39d3ac63ba07dce92
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2025-08-14 10:56:16 -0300

    Estrutura de componentes incial

--- form-multi-step/multistep_form_react/src/App.jsx
+++ form-multi-step/multistep_form_react/src/App.jsx
@@ -1,10 +1,32 @@
+// Components
+import {GrFormNext, GrFormPrevious} from 'react-icons/gr'
+
 import './App.css'
 
 function App() {
 
   return (
-    <div className='App'>
-      <h1>React Multistep</h1>
+    <div className='app'>
+      <div className="header">
+        <h2>Deixe sua avaliação</h2>
+        <p>Ficamos felizes com a sua compra, utilize o formulario abaixo para avaliar o produto</p>
+      </div>
+      <div className="form-container">
+        <p>etapas</p>
+        <form>
+          <div className="inputs-container"></div>
+          <div className="action">
+            <button type='button'>
+              <GrFormPrevious/>
+              <span>Voltar</span>
+            </button>
+            <button type='submit'>
+              <span>Avançar</span>
+              <GrFormNext/>
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
