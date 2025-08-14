08e774371d2a3cb503a9cf9b045a79d751105b48
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2025-08-14 13:43:10 -0300

    Persistindo dados entre etapas

--- form-multi-step/multistep_form_react/src/components/Thanks.jsx
+++ form-multi-step/multistep_form_react/src/components/Thanks.jsx
@@ -3,18 +3,27 @@ import { BsFillEmojiHeartEyesFill, BsFillEmojiSmileFill, BsFillEmojiNeutralFill,
 
 import './Thanks.css'
 
-const Thanks = () => {
+const emojiData = {
+  unsatisfied: <BsFillEmojiFrownFill />,
+  neutral: <BsFillEmojiNeutralFill />,
+  satisfied: <BsFillEmojiSmileFill />,
+  very_satisfied: <BsFillEmojiHeartEyesFill />,
+}
+
+const Thanks = ({ data }) => {
   return (
     <div className='thanks-container'>
       <h2>Falta pouco...</h2>
       <p>A sua opinião é muito importante, em breve você receberá um cupom de 10% de desconto para a sua próxima compra.</p>
       <p>Para concluir a avaliação clique no botão de Enviar abaixo </p>
-      <h3>Aqui está o resumo da sua avaliação:</h3>
+      <h3>Aqui está o resumo da sua avaliação: {data.name}</h3>
       <p className="review-data">
         <span>Satisfação com o produto:</span>
+        {emojiData[data.review]}
       </p>
       <p className="review-data">
         <span>Comentário:</span>
+        {data.comment}
       </p>
     </div>
   )
--- form-multi-step/multistep_form_react/src/components/UserForm.jsx
+++ form-multi-step/multistep_form_react/src/components/UserForm.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 
-const UserForm = () => {
+const UserForm = ({ data, updateFieldHandler }) => {
     return (
         <div>
             <div className="form-control">
                 <label htmlFor="name">Nome:</label>
-                <input type="text" name="name" id="name" placeholder='Digite o seu nmome' required />
+                <input type="text" name="name" id="name" placeholder='Digite o seu nmome' required value={data.name || ""} onChange={(e) => updateFieldHandler("name", e.target.value)}/>
             </div>
-             <div className="form-control">
+            <div className="form-control">
                 <label htmlFor="name">E-mail:</label>
-                <input type="email" name="email" id="email" placeholder='Digite o seu E-mail' required />
+                <input type="email" name="email" id="email" placeholder='Digite o seu E-mail' required value={data.email || ""} onChange={(e) => updateFieldHandler("email", e.target.value)}/>
             </div>
         </div>
     )
 }
 
 export default UserForm
+
