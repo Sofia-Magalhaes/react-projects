ff396e4ed5f1588d68fe17d031bea7c737137163
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2025-08-21 10:56:18 -0300

    Resgate de dados

--- http_react/http_react/src/App.jsx
+++ http_react/http_react/src/App.jsx
@@ -1,10 +1,34 @@
+import { useState, useEffect } from 'react'
+
+const url = "http://localhost:3000/products"
+
 import './App.css'
 
-function App() {  
+function App() {
 
-  return (    
+  // 1 - Resgatando dados
+  const [products, setProducts] = useState([])
+  useEffect(() => {
+
+    async function getData() {
+      const res = await fetch(url)
+      const data = await res.json()
+      setProducts(data)
+    }
+    getData()
+  }, [])
+
+  return (
     <div className="App">
       <h1>HTTP em React</h1>
+      {/* Resgata de dados */}
+      <ul>
+        {products.map((product) =>(
+          <li key={product.id}>
+            {product.name} - R${product.price}
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
